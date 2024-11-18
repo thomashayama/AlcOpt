4d62d03566539a37997aad0d78100f8571935967
Thomas Hayama <thomashayama@gmail.com>
2024-11-18 05:49:31 +0000

    added unum

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.11-slim-buster
 
-RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+RUN apt-get update && apt-get install ffmpeg libsm6 libxext6 git -y
 
 WORKDIR /app
 
 COPY . /alcopt
+
+RUN pip install -r /alcopt/requirements.txt
 RUN pip install /alcopt
 
 # CMD [ "python", "alcopt/run_app.py" ]
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
+# Read requirements from requirements.txt and filter out Git URLs
 with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+    all_requirements = f.read().splitlines()
+    requirements = [req for req in all_requirements if not req.startswith("git+")]
 
 setup(
     name="alcopt",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
